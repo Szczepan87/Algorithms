8e149ec3ecfabfb6c3a8dff3205baa1d4564d7e4
Łukasz Szczepański <l.szczepan@wp.pl>
2018-06-28 16:16:10 +0200

    cd ćwiczeń

--- src/main/java/AlgorytmyCwiczenia/ArraysExc/SortOddsInArray.java
+++ src/main/java/AlgorytmyCwiczenia/ArraysExc/SortOddsInArray.java
@@ -1,4 +1,26 @@
 package AlgorytmyCwiczenia.ArraysExc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SortOddsInArray {
+    public static int[] sortArray(int[] array) {
+
+        List<Integer> oddsToSort = new ArrayList<>();
+        List<Integer> indexesOfEvens = new ArrayList<>();
+
+
+
+        if (array == null || array.length == 0)
+            return array;
+
+        for (int i = 0; i < array.length; i++) {
+            if (i%2!=0)
+                oddsToSort.add(array[i]);
+            else
+                indexesOfEvens.add(i);
+        }
+
+        return array;
+    }
 }
