f0f70374ba90f8c3f848081717d32d5a42252c51
Łukasz Szczepański <l.szczepan@wp.pl>
2018-06-28 10:38:58 +0200

    cd ćwiczeń

---
+++ src/main/java/AlgorytmyCwiczenia/RockPaperScissors.java
@@ -0,0 +1,20 @@
+package AlgorytmyCwiczenia;
+
+public class RockPaperScissors {
+
+    public static String rps(String p1, String p2) {
+
+        String rock = "rock";
+        String paper = "paper";
+        String scissors = "scissors";
+        String p1Won = "Player 1 won!";
+        String p2Won = "Player 2 won!";
+        String draw = "Draw!";
+
+        if (p1.equals(rock) && p2.equals(scissors)) return p1Won;
+        else if (p1.equals(paper) && p2.equals(rock)) return p1Won;
+        else if (p1.equals(scissors) && p2.equals(paper)) return p1Won;
+        else if (p1.equals(p2)) return draw;
+        else return p2Won;
+    }
+}
