3eda3677f60b91078ef9907410634ba31927e272
Łukasz Szczepański <l.szczepan@wp.pl>
2018-07-04 15:35:36 +0200

    cd ćwiczeń

---
+++ src/main/java/AlgorytmyCwiczenia/ArraysExc/CommonAvg.java
@@ -0,0 +1,13 @@
+package AlgorytmyCwiczenia.ArraysExc;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class CommonAvg {
+    /**
+     * Zwraca tablicę par różnych liczb, które mają taką samą średnią arytmetyczną.
+     */
+    public static int[] findIn(int[] arr) {
+
+        throw new NotImplementedException();
+    }
+}
---
+++ src/test/java/AlgorytmyCwiczenia/ArraysExc/CommonAvgTest.java
@@ -0,0 +1,13 @@
+package AlgorytmyCwiczenia.ArraysExc;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CommonAvgTest {
+
+    @Test
+    public void findIn() {
+        fail();
+    }
+}
