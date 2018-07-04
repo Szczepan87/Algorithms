44c3fd473371082ea8367fe2d972f2ec719b3c4d
Łukasz Szczepański <l.szczepan@wp.pl>
2018-07-04 09:01:29 +0200

    cd ćwiczeń

---
+++ src/main/java/AlgorytmyCwiczenia/DnaStrand.java
@@ -0,0 +1,22 @@
+package AlgorytmyCwiczenia;
+
+public class DnaStrand {
+
+    public static String makeComplement(String dna) {
+
+        char[] divided = dna.toUpperCase().toCharArray();
+
+        for (int i = 0; i < divided.length; i++) {
+            if (divided[i] == 'A')
+                divided[i] = 'T';
+            else if (divided[i] == 'T')
+                divided[i] = 'A';
+            else if (divided[i] == 'G')
+                divided[i] = 'C';
+            else if (divided[i] == 'C')
+                divided[i] = 'G';
+        }
+
+        return String.valueOf(divided);
+    }
+}
