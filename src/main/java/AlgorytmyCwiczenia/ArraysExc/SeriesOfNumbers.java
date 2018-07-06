c6c597c26320e31215572dac6e2916a236cd8281
Łukasz Szczepański <l.szczepan@wp.pl>
2018-07-06 12:03:27 +0200

    ciag dalszy zmagań

--- src/main/java/AlgorytmyCwiczenia/ArraysExc/SeriesOfNumbers.java
+++ src/main/java/AlgorytmyCwiczenia/ArraysExc/SeriesOfNumbers.java
@@ -1,8 +1,34 @@
 package AlgorytmyCwiczenia.ArraysExc;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class SeriesOfNumbers {
     public static int[] findCommon(int[] arr1, int[] arr2) {
 
-        return new int[0];
+        if (arr1.length<1 || arr2.length<1)
+            return new int[0];
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        List<Integer> arr1List = new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < arr1.length; i++) {
+            arr1List.add(arr1[i]);
+        }
+        for (int element : arr2) {
+            if (arr1List.contains(element))
+                result.add(element);
+        }
+
+        int[] arrResult = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            arrResult[i] = result.get(i);
+        }
+
+        return arrResult;
     }
 }
