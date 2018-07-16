e2da267e72b2e2817bbdbc184fbd5bac85bf286f
Łukasz Szczepański <l.szczepan@wp.pl>
2018-07-16 14:26:24 +0200

    ciag dalszy zmagań

--- src/main/java/AlgorytmyCwiczenia/ArraysExc/SeriesOfNumbers.java
+++ src/main/java/AlgorytmyCwiczenia/ArraysExc/SeriesOfNumbers.java
@@ -7,28 +7,26 @@ import java.util.List;
 public class SeriesOfNumbers {
     public static int[] findCommon(int[] arr1, int[] arr2) {
 
-        if (arr1.length<1 || arr2.length<1)
+        if (arr1.length < 1 || arr2.length < 1)
             return new int[0];
 
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
-
-        List<Integer> arr1List = new ArrayList<>();
         List<Integer> result = new ArrayList<>();
 
-        for (int i = 0; i < arr1.length; i++) {
-            arr1List.add(arr1[i]);
+        if (arr1.length >= arr2.length) {
+            for (int i = 0; i < arr1.length; i++) {
+                for (int j = 0; j < arr2.length; j++) {
+                    if (arr1[i] == arr2[j]) {
+                        if (j < arr2.length-1 && arr1[i] + 1 == arr2[j + 1] && arr1[i + 1] == arr2[j + 1]) {
+                            result.add(arr1[i]);
+                            result.add(arr1[i + 1]);
+                            break;
+                        }
+                    }
+                }
+            }
         }
-        for (int element : arr2) {
-            if (arr1List.contains(element))
-                result.add(element);
-        }
+        int[] resultArr = new int[result.size()];
 
-        int[] arrResult = new int[result.size()];
-        for (int i = 0; i < result.size(); i++) {
-            arrResult[i] = result.get(i);
-        }
-
-        return arrResult;
+        return resultArr;
     }
 }
--- src/test/java/AlgorytmyCwiczenia/ArraysExc/SeriesOfNumbersTest.java
+++ src/test/java/AlgorytmyCwiczenia/ArraysExc/SeriesOfNumbersTest.java
@@ -11,6 +11,9 @@ public class SeriesOfNumbersTest {
         int[] test1 = new int[]{1,5,9,2,5,8,4,5,6,1,2,4,7,8};
         int[] test2 = new int[]{4,5,6,2,8,5,4,8,5,1,2,5,5};
         int[] result = new int[]{4,5,6,1,2};
+        for (int i = 0; i < SeriesOfNumbers.findCommon(test1, test2).length; i++) {
+            System.out.print(i+", ");
+        }
         assertArrayEquals(result, SeriesOfNumbers.findCommon(test1, test2));
     }
 
