01b752e38271f647cd89b06b431b031041dd4948
Łukasz Szczepański <l.szczepan@wp.pl>
2018-07-16 13:47:24 +0200

    ciag dalszy zmagań

--- src/main/java/AlgorytmyCwiczenia/ArraysExc/Unique.java
+++ src/main/java/AlgorytmyCwiczenia/ArraysExc/Unique.java
@@ -12,13 +12,15 @@ public class Unique {
     public static double find(double[] arr) {
 
         double unique = 0;
-        boolean sameNumber = false;
 
+        //nie widzi ostatniego unikalnego elementu
         for (int i = 0; i < arr.length; i++) {
+            boolean sameNumber = false;
 
             for (int j = i + 1; j < arr.length; j++) {
                 if (arr[i] == arr[j]) {
                     sameNumber = true;
+                    arr[i] = 0.0;
                     break;
                 }
             }
@@ -26,7 +28,6 @@ public class Unique {
                 unique = arr[i];
                 break;
             }
-
         }
         return unique;
     }
