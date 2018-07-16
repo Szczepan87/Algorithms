f7c8f32bcb0a21ac62ca0a816f57c56c2736f25b
Łukasz Szczepański <l.szczepan@wp.pl>
2018-07-16 11:37:45 +0200

    ciag dalszy zmagań

--- src/main/java/AlgorytmyCwiczenia/ArraysExc/Frequency.java
+++ src/main/java/AlgorytmyCwiczenia/ArraysExc/Frequency.java
@@ -9,10 +9,15 @@ public class Frequency {
             return count;
 
         for (int i = 0; i < collection.length; i++) {
+            int max = 0;
             for (int j = 0; j < collection.length; j++) {
-                if (j<i && collection[i] == collection[j+1])
-                    count++;
+                if (j<collection.length-1 && collection[i] == collection[j+1]){
+                    max++;
+                    break;
+                }
             }
+            if (count<max)
+                count = max;
         }
 
         return count;
